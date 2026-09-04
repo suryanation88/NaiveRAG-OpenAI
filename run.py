6ec5b07511951e69d0b8b5905bf8a45490170d63
suryanation88 <schologysuryas@gmail.com>
2026-09-04 22:37:34 +0800

    fix: logic run

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
 import os
+import sys
 from src.ingestion.ingestion_pipeline import run_ingestion_pipeline
 
 def start():
+    # Cek apakah ada flag --force
+    force_mode = "--force" in sys.argv
+    
     print("Memeriksa pembaruan dokumen...")
-    run_ingestion_pipeline() 
+    run_ingestion_pipeline(force=force_mode) 
     
     print("Menjalankan SHAVIRA...")
     os.system("python -m app.main")
